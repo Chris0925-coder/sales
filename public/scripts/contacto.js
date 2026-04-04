f54abe4c9b0775f3eef47aa6218b9bedac8cfb9f
Chris0925-coder <christianguardia@outlook.es>
2026-04-04 01:51:55 -0500

    0

---
+++ public/scripts/contacto.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('formula');
+
+
+function form() {
+
+    form.addEventListener('submit', async function(event) {
+        event.preventDefault(); 
+             
+        const formData = new FormData(form);
+
+        if (formData.get('email').length == 0 || formData.get('control').length == 0) {
+            document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:darkred;">Field is required.</span>';
+            return false;
+        } 
+
+          await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: formData.get('email'), 
+                control: formData.get('control'),
+            }),
+          })
+          .then((response) => response.text())
+          .then((data) => console.log(data))
+          .catch((error) => console.error('Error:', error));
+       
+
+
+        alert('Form submitted successfully!');
+
+
+        window.location.href = "https://www.apartamentodealquiler.shop/";
+    });
+}
+
+form();
